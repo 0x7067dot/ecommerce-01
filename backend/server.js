fc9d0d05fe9c67c003ad668a2070b45cb57475cf
0x7067dot <limitbreak.engine@gmail.com>
2026-06-28 10:34:31 +0530

    Added routes and reformatted file structure

--- backend/server.js
+++ backend/server.js
@@ -3,35 +3,16 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.json());
 
-app.post("/api/products", async (req, res) => {
-    console.log(`Request Body: ${req}`);
-    const product = req.body;
-    
-    if(!product.name || !product.price) {
-        return res.status(400).json({message: "Please provide complete details."});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({sucess: true, data: newProduct}); 
-    }
-    catch (error) {
-        console.log(`Error: ${error}`);
-        return res.status(500).json({success: false, message: "Error"});
-    }
-}
-)
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
     connectDB();
